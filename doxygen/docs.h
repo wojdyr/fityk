d77d99fcc5a3c270fc184ca7bb3243c2e684a051
Marcin Wojdyr <wojdyr@gmail.com>
2005-03-04 14:36:50 +0000

    *** empty log message ***

---
+++ doxygen/docs.h
@@ -0,0 +1,29 @@
+/*! \mainpage Documentation of Fityk Source 
+ *
+ * \par
+ * This is a documentation of source for developers. 
+ * If you just want to use the program, see 
+ * <a href="http://www.unipress.waw.pl/~wojdyr/fityk/doc/html/index.html">
+ * user's manual</a>.
+ * \par
+ * Most of the source doesn't have doxygen comments yet, but I'll be adding
+ * it when writting new code or refactoring old one.
+ * \par
+ * You may also look at 
+ * <a href="http://cvs.sourceforge.net/viewcvs.py/fityk/fityk/TODO?view=markup">
+ * TODO</a> file.
+ * \par
+ * Marcin Wojdyr
+ */
+
+
+// A dummy STL definition for doxygen 
+namespace std
+{
+    template<class T> class vector { public: T data; };
+    template<class T> class deque { public: T data; };
+    template<class T> class list { public: T data; };
+    template<class T> class slist { public: T data; };
+    template<class Key, class Data> class map { public: Key key; Data data; };
+} 
+
