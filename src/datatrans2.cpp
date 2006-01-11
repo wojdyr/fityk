6886c9c4cb0b2ed52bbbed58459b8ea0a57c37dd
Marcin Wojdyr <wojdyr@gmail.com>
2006-01-11 17:38:00 +0000

    *** empty log message ***

---
+++ src/datatrans2.cpp
@@ -0,0 +1,200 @@
+// This file is part of fityk program. Copyright (C) Marcin Wojdyr
+// $Id$
+
+// big grammars in Spirit take a lot of time and memory to compile
+// so they must be splitted into separate compilation units
+// that's the only reason why this file is not a part of datatrans.cpp
+
+#include "datatrans2.h"
+using namespace datatrans;
+
+namespace datatrans {
+
+//----------------------  Parameterized Functions -------------------------
+class InterpolateFunction : public ParameterizedFunction
+{
+public:
+    InterpolateFunction(vector<fp> &params) {
+        for (int i=0; i < size(params) - 1; i+=2)
+            bb.push_back(B_point(params[i], params[i+1]));
+    }
+
+    fp calculate(fp x) { return get_linear_interpolation(bb, x); }
+
+private:
+    std::vector<B_point> bb;
+};
+
+
+class SplineFunction : public ParameterizedFunction
+{
+public:
+    SplineFunction(vector<fp> &params) {
+        for (int i=0; i < size(params) - 1; i+=2)
+            bb.push_back(B_point(params[i], params[i+1]));
+        prepare_spline_interpolation(bb);
+    }
+
+    fp calculate(fp x) { return get_spline_interpolation(bb, x); }
+
+private:
+    std::vector<B_point> bb;
+};
+
+//----------------------   
+
+//----------------------   functors   -------------------------
+
+void push_double::operator()(const double& n) const
+{
+    code.push_back(OP_NUMBER);
+    code.push_back(size(numbers));
+    numbers.push_back(n);
+}
+
+void push_op::push() const 
+{ 
+    code.push_back(op); 
+    if (op2)
+        code.push_back(op2); 
+}
+
+void parameterized_op::push() const
+{ 
+    //DT_DEBUG("PARAMETERIZED " + S(op))  
+    typedef vector<int>::iterator viit;
+    viit first = find(code.begin(), code.end(), OP_PLIST_BEGIN);
+    viit last = find(code.begin(), code.end(), OP_PLIST_END) + 1;
+    vector<fp> params;
+    for (viit i=first; i != last; ++i) 
+        if (*i == OP_NUMBER) 
+            params.push_back(numbers[*++i]);
+    code.erase(first, last);
+    code.push_back(OP_PARAMETERIZED); 
+    code.push_back(size(parameterized));
+    ParameterizedFunction *func = 0;
+    switch (op) {
+        case PF_INTERPOLATE:
+            //TODO shared_ptr
+            func = new InterpolateFunction(params);
+            break;
+        case PF_SPLINE:
+            func = new SplineFunction(params);
+            break;
+        default:
+            assert(0);
+    }
+    parameterized.push_back(func);
+}
+
+} //namespace
+
+template <typename ScannerT>
+DataExpressionGrammar::definition<ScannerT>::definition(
+                                          DataExpressionGrammar const& /*self*/)
+{
+    rprec5 
+        =   DataE2G
+        |   ('-' >> DataE2G) [push_op(OP_NEG)]
+        |   ('+' >> DataE2G)
+        ;
+
+    rprec4 
+        =   rprec5 
+            >> *(
+                  ('^' >> rprec5) [push_op(OP_POW)]
+                )
+        ;
+        
+    rprec3 
+        =   rprec4
+            >> *(   ('*' >> rprec4)[push_op(OP_MUL)]
+                |   ('/' >> rprec4)[push_op(OP_DIV)]
+                |   ('%' >> rprec4)[push_op(OP_MOD)]
+                )
+        ;
+
+    rprec2 
+        =   rprec3
+            >> *(   ('+' >> rprec3) [push_op(OP_ADD)]
+                |   ('-' >> rprec3) [push_op(OP_SUB)]
+                )
+        ;
+
+    rbool
+    // a hack for handling 10 < x < 20 
+    // how it works:
+    //  3 < 5.2
+    //    op:    OP_NUMBER(3)    OP_NUMBER(5.2)   OP_LT
+    //    stack:    ... 3         ... 3 5.2      ... 1 5.2
+    //    stack end:    ^                 ^          ^
+    //  3 < 5.2 < 4 (continued previous example)
+    //    op:     OP_AND     OP_NCMP_HACK  OP_NUMBER(4) OP_LT  OP_AFTER_AND
+    //    stack: ... 1 5.2   ... 5.2       ... 5.2 4   ... 0 4     (flag)
+    //    s. end:  ^               ^               ^       ^  
+        = rprec2
+           >> !(( ("<=" >> rprec2) [push_op(OP_LE)]
+                | (">=" >> rprec2) [push_op(OP_GE)]
+                | ((str_p("==")|"=") >> rprec2)  [push_op(OP_EQ)]
+                | ((str_p("!=")|"<>") >> rprec2)  [push_op(OP_NEQ)]
+                | ('<' >> rprec2)  [push_op(OP_LT)]
+                | ('>' >> rprec2)  [push_op(OP_GT)]
+                )
+                >> *( (str_p("<=")[push_op(OP_AND, OP_NCMP_HACK)] 
+                        >> rprec2) [push_op(OP_LE, OP_AFTER_AND)]
+                    | (str_p(">=")[push_op(OP_AND, OP_NCMP_HACK)] 
+                        >> rprec2) [push_op(OP_GE, OP_AFTER_AND)]
+                    | ((str_p("==")|"=")[push_op(OP_AND, OP_NCMP_HACK)] 
+                        >> rprec2) [push_op(OP_EQ, OP_AFTER_AND)]
+                    | ((str_p("!=")|"<>")[push_op(OP_AND, OP_NCMP_HACK)] 
+                        >> rprec2)  [push_op(OP_NEQ, OP_AFTER_AND)]
+                    | (ch_p('<')[push_op(OP_AND, OP_NCMP_HACK)] 
+                        >> rprec2) [push_op(OP_LT, OP_AFTER_AND)]
+                    | (ch_p('>')[push_op(OP_AND, OP_NCMP_HACK)] 
+                        >> rprec2) [push_op(OP_GT, OP_AFTER_AND)]
+                    )
+               )
+        ;
+
+    rbool_not
+        =  (as_lower_d["not"] >> rbool) [push_op(OP_NOT)]
+        |  rbool
+        ;
+
+    rbool_and
+        =  rbool_not
+           >> *(
+                as_lower_d["and"] [push_op(OP_AND)] 
+                >> rbool_not
+               ) [push_op(OP_AFTER_AND)]
+        ;
+
+    rbool_or  
+        =  rbool_and 
+           >> *(
+                as_lower_d["or"] [push_op(OP_OR)] 
+                >> rbool_and
+               ) [push_op(OP_AFTER_OR)]
+        ;  
+
+
+    rprec1
+        =  rbool_or 
+           >> !(ch_p('?') [push_op(OP_TERNARY)] 
+                >> rbool_or 
+                >> ch_p(':') [push_op(OP_TERNARY_MID)] 
+                >> rbool_or
+               ) [push_op(OP_AFTER_TERNARY)]
+        ;
+}
+
+// explicit template instantiations 
+template DataExpressionGrammar::definition<scanner<char const*, scanner_policies<skipper_iteration_policy<iteration_policy>, match_policy, no_actions_action_policy<action_policy> > > >::definition(DataExpressionGrammar const&);
+
+template DataExpressionGrammar::definition<scanner<char const*, scanner_policies<skipper_iteration_policy<iteration_policy>, match_policy, no_actions_action_policy<no_actions_action_policy<action_policy> > > > >::definition(DataExpressionGrammar const&);
+
+template DataExpressionGrammar::definition<scanner<char const*, scanner_policies<skipper_iteration_policy<iteration_policy>, match_policy, action_policy> > >::definition(DataExpressionGrammar const&);
+
+DataExpressionGrammar DataExpressionG;
+
+
---
+++ src/datatrans2.h
@@ -0,0 +1,128 @@
+// This file is part of fityk program. Copyright (C) Marcin Wojdyr
+// $Id$
+
+#ifndef FITYK__DATATRANS2__H__
+#define FITYK__DATATRANS2__H__
+
+// big grammars in Spirit take a lot of time and memory to compile
+// so they must be splitted into separate compilation units
+// that's the only reason why this file is not a part of datatrans.cpp
+// code here was originally part of datatrans.cpp (yes, .cpp)
+//
+// this file is included only by datatrans*.cpp
+
+#include "datatrans.h"
+#include "common.h"
+#include "data.h"
+#include "var.h"
+#include "numfuncs.h"
+#include "logic.h"
+#include <boost/spirit/core.hpp>
+
+using namespace std;
+using namespace boost::spirit;
+
+namespace datatrans {
+
+class ParameterizedFunction
+{
+public:
+    ParameterizedFunction() {}
+    virtual ~ParameterizedFunction() {}
+    virtual fp calculate(fp x) = 0;
+};
+
+
+extern vector<int> code;        //  VM code 
+extern vector<double> numbers;  //  VM data 
+extern vector<ParameterizedFunction*> parameterized; // also used by VM 
+extern const int stack_size;  //should be enough, 
+                              //there are no checks for stack overflow  
+
+inline bool x_lt(const Point &p1, const Point &p2) { return p1.x < p2.x; }
+inline bool x_gt(const Point &p1, const Point &p2) { return p1.x > p2.x; }
+inline bool y_lt(const Point &p1, const Point &p2) { return p1.y < p2.y; }
+inline bool y_gt(const Point &p1, const Point &p2) { return p1.y > p2.y; }
+inline bool sigma_lt(const Point &p1, const Point &p2) 
+                                              { return p1.sigma < p2.sigma; }
+inline bool sigma_gt(const Point &p1, const Point &p2) 
+                                              { return p1.sigma > p2.sigma; }
+inline bool active_lt(const Point &p1, const Point &p2) 
+                                        { return p1.is_active < p2.is_active; }
+inline bool active_gt(const Point &p1, const Point &p2) 
+                                        { return p1.is_active > p2.is_active; }
+
+// operators used in VM code
+enum DataTransformVMOperator
+{
+    OP_NEG=1,   OP_EXP,   OP_SIN,   OP_COS,  OP_ATAN,  OP_ABS,  OP_ROUND, 
+    OP_TAN/*!*/, OP_ASIN, OP_ACOS,
+    OP_LOG10, OP_LN,  OP_SQRT,  OP_POW,   //these functions can set errno    
+    OP_ADD,   OP_SUB,   OP_MUL,   OP_DIV/*!*/,  OP_MOD,
+    OP_MIN,   OP_MAX,     
+    OP_VAR_X, OP_VAR_Y, OP_VAR_S, OP_VAR_A, 
+    OP_VAR_x, OP_VAR_y, OP_VAR_s, OP_VAR_a, 
+    OP_VAR_n, OP_VAR_M, OP_NUMBER,  
+    OP_OR, OP_AFTER_OR, OP_AND, OP_AFTER_AND, OP_NOT,
+    OP_TERNARY, OP_TERNARY_MID, OP_AFTER_TERNARY, OP_DELETE_COND,
+    OP_GT, OP_GE, OP_LT, OP_LE, OP_EQ, OP_NEQ, OP_NCMP_HACK, 
+    OP_RANGE, OP_INDEX,
+    OP_ASSIGN_X, OP_ASSIGN_Y, OP_ASSIGN_S, OP_ASSIGN_A,
+    OP_DO_ONCE, OP_RESIZE, OP_ORDER, OP_DELETE, OP_BEGIN, OP_END, 
+    OP_SUM, OP_IGNORE, 
+    OP_PARAMETERIZED, OP_PLIST_BEGIN, OP_PLIST_END
+};
+
+// parametrized functions
+enum {
+    PF_INTERPOLATE, PF_SPLINE
+};
+
+//-- functors used in the grammar for putting VM code and data into vectors --
+
+struct push_double
+{
+    void operator()(const double& n) const;
+};
+
+struct push_the_double: public push_double
+{
+    push_the_double(double d_) : d(d_) {}
+    void operator()(char const*, char const*) const 
+                                               { push_double::operator()(d); }
+    void operator()(const char) const { push_double::operator()(d); }
+    double d;
+};
+
+struct push_the_var: public push_double
+{
+    void operator()(char const* a, char const* b) const 
+     { push_double::operator()(AL->find_variable(string(a+1,b))->get_value()); }
+};
+
+struct push_op
+{
+    push_op(int op_, int op2_=0) : op(op_), op2(op2_) {}
+    void push() const;
+    void operator()(char const*, char const*) const { push(); }
+    void operator()(char) const { push(); }
+
+    int op, op2;
+};
+
+
+struct parameterized_op
+{
+    parameterized_op(int op_) : op(op_) {}
+
+    void push() const;
+    void operator()(char const*, char const*) const { push(); }
+    void operator()(char) const { push(); }
+
+    int op;
+};
+
+
+} //namespace
+
+#endif 
