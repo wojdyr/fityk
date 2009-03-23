a8a32d75d3022fd927183b9879e21ad267dd0bdc
Marcin Wojdyr <wojdyr@gmail.com>
2009-03-23 14:25:24 +0000

    year changed

--- src/wxgui/dialogs.cpp
+++ src/wxgui/dialogs.cpp
@@ -581,7 +581,7 @@ AboutDlg::AboutDlg(wxWindow* parent)
                     + wxT("\n"));
     txt->SetDefaultStyle(wxTextAttr(wxNullColour, wxNullColour, 
                                     *wxNORMAL_FONT));
-    txt->AppendText(wxT("\nCopyright (C) 2001 - 2008 Marcin Wojdyr\n\n"));
+    txt->AppendText(wxT("\nCopyright (C) 2001 - 2009 Marcin Wojdyr\n\n"));
     txt->SetDefaultStyle(wxTextAttr(*wxBLUE));
     txt->AppendText(wxT("http://www.unipress.waw.pl/fityk/\n\n"));
     txt->SetDefaultStyle(wxTextAttr(*wxBLACK));
