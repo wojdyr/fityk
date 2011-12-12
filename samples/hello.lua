2c97ffbebb065cb2a30add096a3bc040ea706989
Marcin Wojdyr <wojdyr@gmail.com>
2011-12-12 09:55:10 +0000

    update and change autotools scripts, move swig/ to src

--- samples/hello.lua
+++ samples/hello.lua
@@ -1,36 +1,21 @@
-#!/usr/bin/env lua
-
--- First, check if the script is called from stand-alone Lua interpreter.
--- The interpreter embedded in libfityk sets global F (an instance of
+-- Sample of Lua scripting in Fityk.
+--
+-- Lua interpreter embedded in libfityk sets global F (an instance of
 -- fityk.Fityk) which can be used to access and change the state of the program.
--- In other Lua interpreters we must import fityk module (it can be named
--- liblua-fityk to avoid conflicts and you may need to rename it to fityk.so)
-if type(F) ~= 'userdata' or swig_type(F) ~= "fityk::Fityk *" then
-    print("we are NOT inside fityk...")
-    require("fityk")
-    F = fityk.Fityk()
-    -- in fityk messages from the program and from F:out go to the output
-    -- window, in cfityk to console output, in stand-alone Lua we must
-    -- set the output
-    F:redir_messages(io.stdout)
-    -- the output can be disabled using redir_messages(nil)
-end
+print("swig_type(F): ", swig_type(F)) -- -> stdout
 
--- basic checks
-print("fityk is", swig_type(fityk)) -- table
-print("fityk.Fityk is", swig_type(fityk.Fityk)) -- function
-
-F:out("libfityk version: " .. F:get_info("version"))
+F:out("libfityk version: " .. F:get_info("version")) -- -> fityk output
 F:out("ln(2) = " .. F:calculate_expr("ln(2)"))
 
--- check error handling
+F:out("Testing error handling...")
 status, err = pcall(function() F:execute("fit") end)
 if status == false then
-    F:out("Error caught: " .. err)
+    F:out("OK. Error caught: " .. err)
 end
 
+-- try to find directory of this file
 if type(arg) == 'table' then
-    directory = string.match(arg[0], "^.*[/\\]")
+    directory = string.match(arg[0], "^.*[/\\]") or ""
 else
     directory = ""
 end
