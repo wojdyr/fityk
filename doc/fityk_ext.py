4206cda9d4204532974b705b7a04b6accadeac4c
Marcin Wojdyr <wojdyr@gmail.com>
2012-01-29 11:54:38 +0000

    docs: tweaking the RTD hack from previous commit

--- doc/fityk_ext.py
+++ doc/fityk_ext.py
@@ -46,9 +46,9 @@ def setup(app):
 
 # this is hack is needed to use our layout.html on ReadTheDocs
 from sphinx.jinja2glue import BuiltinTemplateLoader
+from jinja2 import TemplateNotFound
 class MyTemplateLoader(BuiltinTemplateLoader):
     def get_source(self, environment, template):
-        print "(MyTemplateLoader.get_source) searching for",  template
         # If template name in Jinja's "extends" is prepended with "!"
         # Sphinx skips project's template paths.
         # In BuiltinTemplateLoader self.templatepathlen is used to remove
@@ -56,7 +56,16 @@ class MyTemplateLoader(BuiltinTemplateLoader):
         # This hack should leave the last path, so "!layout.html" will find
         # the template from Fityk. To avoid recursion, Fityk template
         # is not using "!".
-        self.templatepathlen -= 1
-        return BuiltinTemplateLoader.get_source(self, environment, template)
-        self.templatepathlen += 1
-
+        print("\n(MyTemplateLoader.get_source) searching for %s" % template)
+        loaders = self.loaders
+        # exclamation mark starts search from theme
+        if template.startswith('!'):
+            loaders = loaders[self.templatepathlen-1:]
+            template = template[1:]
+        for loader in loaders:
+            print("\ttrying in: %s" % ":".join(loader.searchpath))
+            try:
+                return loader.get_source(environment, template)
+            except TemplateNotFound:
+                pass
+        raise TemplateNotFound(template)
