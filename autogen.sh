1ad35aba10354b69bb4f053d3292604a2e292318
Marcin Wojdyr <wojdyr@gmail.com>
2012-10-28 01:54:01 +0100

    patch cmpfit so it can run 0 iterations (only calculate initial stats)

--- autogen.sh
+++ autogen.sh
@@ -10,8 +10,8 @@ set -x
 # from http://www.physics.wisc.edu/~craigm/idl/cmpfit.html
 curl -O http://www.physics.wisc.edu/~craigm/idl/down/cmpfit-1.2.tar.gz
 tar xzf cmpfit-1.2.tar.gz
-mkdir -p fityk/cmpfit/
 cp -af cmpfit-1.2/mpfit.* fityk/cmpfit/
+(cd fityk/cmpfit/ && patch -p0 <cmpfit.patch)
 
 autoreconf --install --verbose  \
 && ./configure "$@"
