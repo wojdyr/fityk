754157bd00fcdf04dacd3f6d74b34f3239fd4b36
Marcin Wojdyr <wojdyr@gmail.com>
2013-04-12 04:18:38 +0100

    added modified copy of cmpfit-1.2 to the repo (for easier building)

--- autogen.sh
+++ autogen.sh
@@ -6,13 +6,6 @@ set -x
 (cd doc && make)                  # build docs
 #mkdir -p doc/html/placeholder    # do not build docs
 
-# download and unpack cmpfit-1.2.tar.gz
-# from http://www.physics.wisc.edu/~craigm/idl/cmpfit.html
-curl -O http://www.physics.wisc.edu/~craigm/idl/down/cmpfit-1.2.tar.gz
-tar xzf cmpfit-1.2.tar.gz
-cp -af cmpfit-1.2/mpfit.* fityk/cmpfit/
-(cd fityk/cmpfit/ && patch -p0 <cmpfit.patch)
-
 autoreconf --install --verbose  \
 && ./configure "$@"
 
