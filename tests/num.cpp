c60e6cb8c46549d16b257467764cfb7f83257e9e
Marcin Wojdyr <wojdyr@gmail.com>
2015-04-01 19:21:38 +0100

    tests/num.cpp: one more test

--- tests/num.cpp
+++ tests/num.cpp
@@ -32,7 +32,6 @@ TEST_CASE("invert-matrix-4x4", "") {
         REQUIRE(mat[i] == Approx(a[i]));
 }
 
-/*
 TEST_CASE("invert-matrix-with-zeros", "") {
     const double a[9] = { 4., 0., 7.,
                           0., 0., 0.,
@@ -44,5 +43,27 @@ TEST_CASE("invert-matrix-with-zeros", "") {
     invert_matrix(mat, 3);
     for (size_t i = 0; i != mat.size(); ++i)
         REQUIRE(mat[i] == Approx(a_inv[i]));
+}
+
+/*
+TEST_CASE("pseudo-inverse", "") {
+    const double a[16] = {
+         1.,  2.,  3.,  4.,
+        -1.,  0.,  1.,  2.,
+         1.,  2.,  3.,  4., // same as 1st row
+         1.,  2.,  1.,  0. };
+    const double a_inv[16] = {
+         0.25 , -1.    ,  0.25 , -0.5       ,
+        -0.125,  5./12., -0.125,  2./3.,
+         0.   ,  1./6. ,  0.   ,  1./6.,
+         0.125, -1./12.,  0.125, -1./3. };
+    //a . a_inv = almost I, rows 1 and 3 are avaraged [.5 0 .5 0]
+    vector<realt> mat(a, a+16);
+    invert_matrix(mat, 4);
+    for (size_t i = 0; i != mat.size(); ++i)
+        REQUIRE(mat[i] == Approx(a_inv[i]));
+    invert_matrix(mat, 4);
+    for (size_t i = 0; i != mat.size(); ++i)
+        REQUIRE(mat[i] == Approx(a[i]));
 }
 */
